85b88035ccac8ac237284d1f94de8090657da398
Damian Ciasnocha <daneek80@gmail.com>
2019-07-13 06:39:54 +0100

    Add files via upload

--- one/main.js
+++ one/main.js
@@ -1,3 +1,31 @@
 let button = document.getElementById("enter");
 let input = document.getElementById("userinput");
 let ul = document.querySelector("ul");
+
+// function declarations //
+
+function inputLength(){
+    return input.value.length
+}
+
+function createListElement() {
+    let li = document.createElement("li");
+    li.appendChild(document.createTextNode(input.value));
+    ul.appendChild(li);
+    input.value= "";
+}
+
+function addListAfterClick() {
+    if(inputLength()>0){
+        createListElement();
+    }
+}
+
+function addListAfterKeypress(){
+    if(inputLength()>0 && event.keyCode === 13) {
+        createListElement();
+    }
+}
+
+button.addEventListener("click",addListAfterClick);
+input.addEventListener("keypress",addListAfterKeypress);
